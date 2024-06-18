5adcf6f811a4df48ce756a710072e8d281f92c25
narwa <narwandaru.nara@gmail.com>
2024-06-19 02:13:18 +0700

    feat: migrations for main models

---
+++ database/migrations/2024_06_18_000004_create_reports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->bigIncrements('report_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('status', 120);
+            $table->string('context_type', 16);
+            $table->unsignedInteger('context_id');
+            $table->string('report_details', 255);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
