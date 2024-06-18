667608cefb249516540fb51960f96b4d2df4c710
narwa <narwandaru.nara@gmail.com>
2024-06-19 04:21:39 +0700

    feat: created factories

---
+++ database/factories/ReportFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Shop;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Report>
+ */
+class ReportFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'status' => 'pending',
+            'context_type' => 'shop',
+            'context_id' => Shop::factory(),
+            'report_details' => $this->faker->sentence,
+        ];
+    }
+}
