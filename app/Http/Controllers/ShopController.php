b1ee34747fade277050259924a26fa9e09e5f459
narwa <narwandaru.nara@gmail.com>
2024-06-19 17:07:39 +0700

    feat: basic resourceful methods

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Shop;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ShopController extends Controller
 {
@@ -18,9 +19,20 @@ class ShopController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $user = Auth::user();
+
+        // Check whether the shop already exists with the same user
+        $existingShop = Shop::where('user_id', $user->getAuthIdentifier())->first();
+
+        if ($existingShop) {
+            return response()->json([
+                'message' => 'Shop already exists for the user',
+            ], 400);
+        }
+
+        return view('shop.create');
     }
 
     /**
@@ -28,7 +40,33 @@ class ShopController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:50',
+        ]);
+
+        $user = Auth::user();
+
+        // Check whether the shop already exists with the same user
+        $existingShop = Shop::where('user_id', $user->getAuthIdentifier())->first();
+
+        if ($existingShop) {
+            return response()->json([
+                'message' => 'Shop already exists for the user',
+            ], 400);
+        }
+
+        $shop = Shop::create([
+            'name' => $validated['name'],
+            'address' => '',
+            'status' => 'open',
+            'schedules_data' => null,
+            'user_id' => $user->getAuthIdentifier(),
+        ]);
+
+        return response()->json([
+            'message' => 'Shop created successfully',
+            'shop' => $shop,
+        ], 201);
     }
 
     /**
@@ -36,7 +74,13 @@ class ShopController extends Controller
      */
     public function show(Shop $shop)
     {
-        //
+        if (!$shop) {
+            return response()->json([
+                'message' => 'Shop not found',
+            ], 404);
+        }
+
+        return view('shop.show', ['shop' => $shop]);
     }
 
     /**
@@ -44,7 +88,9 @@ class ShopController extends Controller
      */
     public function edit(Shop $shop)
     {
-        //
+        $this->checkOwnership($shop);
+
+        return view('shop.edit', ['shop' => $shop]);
     }
 
     /**
@@ -52,7 +98,21 @@ class ShopController extends Controller
      */
     public function update(Request $request, Shop $shop)
     {
-        //
+        $this->checkOwnership($shop);
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:50',
+            'address' => 'required|string|max:100',
+            'status' => 'required|string|in:open,closed',
+            'schedules_data' => 'nullable|json',
+        ]);
+
+        $shop->update($validated);
+
+        return response()->json([
+            'message' => 'Shop updated successfully',
+            'shop' => $shop,
+        ], 200);
     }
 
     /**
@@ -60,6 +120,45 @@ class ShopController extends Controller
      */
     public function destroy(Shop $shop)
     {
-        //
+        $this->checkOwnership($shop);
+
+        $shop->delete();
+
+        return response()->json([
+            'message' => 'Shop deleted successfully',
+        ], 200);
+    }
+
+    // Helper methods
+
+    /**
+     * Check for shop ownership.
+     */
+    private function checkOwnership(Shop $shop) {
+        $user = Auth::user();
+
+        if ($shop->user_id !== $user->getAuthIdentifier()) {
+            return response()->json([
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+    }
+
+    // Resource Finders
+
+    /**
+     * Get the shop by the given ID.
+     */
+    public function getShopById(int $shopId): Shop
+    {
+        return Shop::findOrFail($shopId);
+    }
+
+    /**
+     * Get shops that contains the given keyword of name.
+     */
+    public function getShopsByName(string $keyword): array
+    {
+        return Shop::where('name', 'like', "%$keyword%")->get();
     }
 }
