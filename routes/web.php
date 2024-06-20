8d75a435b7fa582e017ac28bc755f5beba315d9e
Nara <98963440+vianneynara@users.noreply.github.com>
2024-06-20 17:32:19 +0700

    refactor: change root default view

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ProductController;
 
 Route::get('/',function() {
-    return view('welcome');
+    return view('homepage');
 });
 
 Route::get('/dashboard', function () {
